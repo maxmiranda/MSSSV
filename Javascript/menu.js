9281a0be62712d0c88d808079548f21777bb1d68
Max Miranda <maxmiranda@berkeley.edu>
2017-06-27 10:57:50 -0400

    Functioning About Page, working through Practice
    We talking bout Practice

--- Javascript/menu.js
+++ Javascript/menu.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
+  $("#news li").hover(
+    function() {
+        $(this).stop().animate({
+          "background-color": "#191919",
+          "border-radius": "20"
+        }, "medium")
+    }, function() {
+        $(this).stop().animate({
+          "background-color": "#F7F7F7",
+          "border-radius": "0"
+        }, "medium")
+    })
   $("nav a").hover(
     function() {
         $(this).stop().animate({
@@ -23,5 +35,15 @@ $(document).ready(function() {
           "opacity": ".43"
         }, "slow")
     })
+  $('#practicemenu li').hover(
+    function() {
+      $(this).stop().animate({
+        "background-size": "120%"
+      }, "slow")
 
+    }, function() {
+      $(this).stop().animate({
+        "background-size": "100%"
+      }, "slow")
+    })
 })
